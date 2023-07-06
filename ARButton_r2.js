85817b67402db0624a969ffdb37a5b8be53b803f
oba-mori <99237421+oba-mori@users.noreply.github.com>
2023-07-06 13:36:00 +0900

    Add files via upload

--- ARButton_r2.js
+++ ARButton_r2.js
@@ -8,18 +8,35 @@ class ARButton {
   createButton() {
     const button = document.createElement('button');
     button.textContent = 'ボタン';
+
+
+		// ボタンクリックイベント
     button.addEventListener('click', () => {
+			//変数値を変更する関数
       this.incrementValue();
+
+			//表示
       console.log('ボタンクリック後の値:', this.value);
+
+			// 
       this.dispatchValueChangedEvent();
     });
     return button;
   }
 
+	//変数値を変更する関数
   incrementValue() {
-    this.value++;
-  }
+		if (this.value == 0)
+		{
+  	  this.value = 1;
+		}
+		else
+		{
+			this.value = 0;
+		}
+	}
 
+	// 
   addValueChangedCallback(callback) {
     this.valueChangedCallbacks.push(callback);
   }
