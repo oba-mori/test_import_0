732985aca531f3e49f4d5952feddd418bfb78a55
oba-mori <99237421+oba-mori@users.noreply.github.com>
2023-07-06 13:13:29 +0900

    Add files via upload

--- ARButton_r2.js
+++ ARButton_r2.js
@@ -1,5 +1,15 @@
 // 
 class ARButton {
-}
 
+	
+  constructor() {
+    this.value = 0;
+  }
+
+  incrementValue() {
+    this.value += 1;
+    console.log('Bで変更された値:', this.value);
+  }
+
+}
 export { ARButton };
